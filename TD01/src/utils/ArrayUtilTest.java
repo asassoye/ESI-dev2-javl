454258e4cdd3f97d1de93fe7c911c5a27601db2b
Andrew Sassoye <andrew@sassoye.be>
2020-02-04 11:50:45 +0100

    TD01: copy testSwapOutsideArrayNegative + JUnit 4 to 5

--- TD01/src/utils/ArrayUtilTest.java
+++ TD01/src/utils/ArrayUtilTest.java
@@ -1,8 +1,9 @@
 package g54327.dev2.recap.utils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ArrayUtilTest {
     @Test
@@ -20,5 +21,21 @@ public class ArrayUtilTest {
         //Assert
         int[] expected = {11, 10, 12};
         assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testSwapOutsideArrayNegative() {
+        System.out.println("swap position négative");
+
+        //Arrange
+        int[] array = {10, 11, 12};
+        int pos1 = -1;
+        int pos2 = 0;
+
+        //Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //Action
+            ArrayUtil.swap(array, pos1, pos2);
+        });
     }
 }
