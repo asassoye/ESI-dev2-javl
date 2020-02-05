d3f7abf03ba04dc271ca3d2996fc40671d0d5184
Andrew Sassoye <andrew@sassoye.be>
2020-02-05 11:10:54 +0100

    TD01: javadoc

--- TD01/src/Memory.java
+++ TD01/src/Memory.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020 Andrew SASSOYE
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package g54327.dev2.recap;
 
 import g54327.dev2.recap.utils.ArrayUtil;
@@ -5,7 +24,19 @@ import g54327.dev2.recap.utils.MemoryUtil;
 
 import java.util.Scanner;
 
+/**
+ * Memory class
+ *
+ * @author Andrew SASSOYE <andrew@sassoye.be>
+ */
 public class Memory {
+    /**
+     * Initialise set of cards
+     *
+     * @param n number of pairs, between 3 and 20 included.
+     * @return array of cards
+     * @throws IllegalArgumentException if n of pairs is not between 3 and 20.
+     */
     public static int[] initCards(int n) {
         if (n < 3 || n > 20) {
             throw new IllegalArgumentException("Le nombre de cartes doit être compris entre 3 et 20 inclus");
@@ -21,6 +52,14 @@ public class Memory {
         return cards;
     }
 
+    /**
+     * Ask the player for a card position. Ask until a good answer is given.
+     *
+     * @param cards Array of cards
+     * @param collectedCards Array of collected cards
+     *
+     * @return The position of the chosen card.
+     */
     public static int askPosition(int[] cards, boolean[] collectedCards) {
         System.out.printf("Entrez une position de carte (0 à %d): ", cards.length - 1);
         int position;
@@ -38,6 +77,16 @@ public class Memory {
         return position;
     }
 
+    /**
+     * Check if the cards at two given positions are equal
+     *
+     * @param cards Array of cards
+     * @param collectedCards Array of collected cards
+     * @param pos1 Position 1 to check
+     * @param pos2 Position 2 to check
+     *
+     * @throws IllegalArgumentException If pos1 is pos2
+     */
     public static void checkPosition(int[] cards, boolean[] collectedCards, int pos1, int pos2) {
         if (pos1 == pos2) {
             throw new IllegalArgumentException("La pos1 doit être differente que pos2");
@@ -52,6 +101,13 @@ public class Memory {
         }
     }
 
+    /**
+     * Checks if the game is over.
+     *
+     * @param collectedCards Array of collected cards.
+     *
+     * @return true if the game is over, false if the game is still in progress.
+     */
     public static boolean isGameOver(boolean[] collectedCards) {
         for (boolean collected :
                 collectedCards) {
@@ -62,6 +118,13 @@ public class Memory {
         return true;
     }
 
+    /**
+     * Play the game
+     *
+     * @param n Number of pairs wished
+     *
+     * @return number of game rounds before finishing the game
+     */
     public static int playMemory(int n) {
         int[] cards = initCards(n);
         ArrayUtil.shuffle(cards);
@@ -81,6 +144,11 @@ public class Memory {
         return tour - 1;
     }
 
+    /**
+     * Game
+     *
+     * @param args Not used
+     */
     public static void main(String[] args) {
         System.out.println("*** Memory ***");
         System.out.print("Avec combien de paires voulez-vous jouer? (3 à 20): ");
@@ -92,6 +160,13 @@ public class Memory {
         System.out.printf("Partie terminée en %d coups\n", tours);
     }
 
+    /**
+     * Asks for an int. Ask again. Asks again until an integer is entered.
+     *
+     * @param notIntError Error to display if the input is incorrect
+     *
+     * @return The asked int
+     */
     private static int askInt(String notIntError) {
         Scanner input = new Scanner(System.in);
         while (!input.hasNextInt()) {
@@ -100,6 +175,17 @@ public class Memory {
         return input.nextInt();
     }
 
+    /**
+     * Asks for an int between two numbers (included). Ask again.
+     * Asks again until a correct integer is entered.
+     *
+     * @param notIntError Error to display if the input is not an int
+     * @param notInIntervalError Error to display if the input is not between the interval
+     * @param pos1 First position in interval
+     * @param pos2 Second position in interval
+     *
+     * @return The asked integer
+     */
     private static int askIntBetween(String notIntError, String notInIntervalError,
                                      int pos1, int pos2) {
         int integer;
