682a0a70c36035b82a66e0b63b33a5f1c31941b6
Andrew Sassoye <andrew@sassoye.be>
2020-02-12 10:59:59 +0100

    TD03: Card class

---
+++ TD03/src/Card.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2020 Andrew SASSOYE
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package g54327.dev2.td03;
+
+/**
+ * Card class
+ * <p>
+ * Abstraction a Card in Memory game
+ *
+ * @author Andrew SASSOYE <andrew@sassoye.be>
+ */
+public class Card {
+    private int value;
+    private boolean hidden;
+
+    /**
+     * Card constructor
+     *
+     * @param value Card value
+     * @throws IllegalArgumentException Value is not positive
+     */
+    public Card(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("Value must be positive!");
+        }
+
+        this.value = value;
+        this.hidden = true;
+    }
+
+    /**
+     * Value getter
+     *
+     * @return Value of card
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Hidden getter
+     *
+     * @return Card hidden or not
+     */
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    /**
+     * Reveal the card
+     */
+    public void reveal() {
+        this.hidden = false;
+    }
+}
